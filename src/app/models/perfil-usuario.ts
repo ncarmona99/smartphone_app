797e5ccf8487f46db28d16f9e3f353ba27c2dd50
mariajosecq <cote21cq@gmail.com>
2024-09-04 17:12:11 -0400

    Se crea carpeta models y las clases iniciales del proyecto

---
+++ src/app/models/perfil-usuario.ts
@@ -0,0 +1,11 @@
+import { Rol } from "./rol";
+import { User } from "./user";
+
+export interface PerfilUsuario{
+    user: User;
+    rol: Rol;
+    nombre: string;
+    apellido: string;
+    correo: string;
+    telefono: number;
+}
